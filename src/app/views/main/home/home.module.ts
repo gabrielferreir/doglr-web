1f26782185e14fd6b01fb1795390778981c82d0a
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-31 12:54:55 -0300

    Colocando tabs

--- src/app/views/main/home/home.module.ts
+++ src/app/views/main/home/home.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { HomeComponent } from './home.component';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import {HomeComponent} from './home.component';
 import {SharedModule} from '../../../shared.module';
 
 @NgModule({
   imports: [
     SharedModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {
+}
