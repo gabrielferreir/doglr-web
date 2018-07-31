d844f3957cd3a150f66dc55b8aa6bbfaeb8afd82
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-07-31 13:47:53 -0300

    Adicionando parametro no componente card-animal

--- src/app/views/main/home/home.component.ts
+++ src/app/views/main/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  joaquina = {
+    id: 'dnhsjdba',
+    name: 'Joaquina',
+    age: '3 anos',
+    breed: 'Vira-lata',
+    image: 'joaquina.png',
+    visualizacoes: 26
+  };
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
