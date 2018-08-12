5ab9769cc06a33105b39f984dd8aa0d7a40c1a20
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2018-08-12 11:27:34 -0700

    Exibindo tabs

--- src/app/views/main/animal/animal.component.ts
+++ src/app/views/main/animal/animal.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Location} from '@angular/common';
+import {environment} from '../../../../environments/environment';
+import {ApiService} from '../../../core/api/api.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-animal',
@@ -7,20 +10,38 @@ import {Location} from '@angular/common';
   styleUrls: ['./animal.component.scss']
 })
 export class AnimalComponent implements OnInit {
-  loading: boolean;
+  loading: boolean = true;
+  info: {};
 
   @ViewChild('images') images;
 
-  constructor(public _location: Location) {
+  constructor(public _location: Location,
+              private _api: ApiService,
+              private _route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    console.log('Route', this._route.snapshot.params.id);
+    this.get();
   }
 
   nextImage() {
     const totalPages = this.images.element.nativeElement.querySelectorAll('.page').length;
     const nextPage = this.images.currentPage < totalPages ? this.images.currentPage + 1 : 1;
     this.images.pagesGoToPage(nextPage);
+  }
+
+  get() {
+    this._api.request('GET', `${environment.API}/animals/${this._route.snapshot.params.id}`, {})
+      .subscribe(res => {
+        console.log(res);
+        this.info = res.content;
+      }, err => {
+        console.log(err);
+
+      }, () => {
+        this.loading = false;
+      });
   }
 
 }
